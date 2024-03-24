9be8c96560a5221356307e2a426816a9a01ba86a
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-24 14:16:02 +0200

    your message here

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <Image src={logo} alt="" className="size-20 float-right" />
       <h1 className="text-2xl">Welcome. How can I help you?</h1>
-      <div className="flex flex-row justify-evenly">
+      <div className="flex flex-row justify-evenly w-full">
         <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-pink-400">
           Mode 1
         </div>
