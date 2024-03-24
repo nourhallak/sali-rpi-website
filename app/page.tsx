5a09a1b10d43a1629a32ec93f2ceb67e72cad5e1
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-24 12:06:41 +0200

    your message here

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,22 @@
 import Image from 'next/image';
 import logo from '../public/logo.jpg';
-
-
+// cards: String;
+// cards="rounded shadow-lg size-48 p-8 border-t-8";
 export default function Home() {
   return (
-    <main className="bg-color-red">
-      <Image src={logo} alt="" className="scale-10" />
+    <main className="">
+      <Image src={logo} alt="" className="h-44" />
       <h1 className="m-16 text-2xl ">Welcome, How can I help you?</h1>
       <div className="flex flex-row justify-evenly">
-
         <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-pink-400">
-
           Mode 1
         </div>
-        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-purple-400 ">
+        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-purple-400">
           Mode 2
         </div>
-        <div className="rounded shadow-lg size-48 p-8  border-t-8 border-t-blue-400">
+        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-blue-400">
           Mode 3
         </div>
-
       </div>
     </main>
   );
