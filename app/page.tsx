7050686dfcd8780240e162cd206d4b1cf964e275
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-24 14:16:30 +0200

    your message here

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import logo from '../public/Logo_1.png';
 export default function Home() {
   return (
     <main>
-      <Image src={logo} alt="" className="size-20 float-right" />
+      <Image src={logo} alt="" className="size-20" />
       <h1 className="text-2xl">Welcome. How can I help you?</h1>
       <div className="flex flex-row justify-evenly w-full">
         <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-pink-400">
