bb58f3039df637bb94b8eedc4378050eb2a7ba32
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-24 14:18:05 +0200

    your message here

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
       <Image src={logo} alt="" className="size-20" />
       <h1 className="text-2xl">Welcome. How can I help you?</h1>
       <div className="flex flex-row justify-evenly w-full">
-        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-pink-400">
+        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-pink-600">
           Mode 1
         </div>
-        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-purple-400">
+        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-purple-600">
           Mode 2
         </div>
-        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-blue-400">
+        <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-blue-600">
           Mode 3
         </div>
       </div>
