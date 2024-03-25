65006f92619ed01b4dcf26cffa0f073a38af7692
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-25 10:51:50 +0200

    Logo change

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image';
-import logo from '../public/Logo_1.png';
+import logo from '../public/Logo_2.png';
 
 export default function Home() {
   return (
     <main>
-      <Image src={logo} alt="" className="size-20" />
-      <h1 className="text-2xl">Welcome. How can I help you?</h1>
-      <div className="flex flex-row justify-evenly w-full">
+      <div className='w-full h-20'>
+        <Image src={logo} alt="" className="w-32 m-6 float-right" />
+      </div>
+      <div className="">
+
+      <h1 className="ml-12 mb-4 w-fit text-2xl">Welcome. How can I help you?</h1>
+      <div className="flex flex-row justify-evenly">
         <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-pink-600">
           Mode 1
         </div>
@@ -16,6 +20,7 @@ export default function Home() {
         <div className="rounded shadow-lg size-48 p-8 border-t-8 border-t-blue-600">
           Mode 3
         </div>
+      </div>
       </div>
     </main>
   );
