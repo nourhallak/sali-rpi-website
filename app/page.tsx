72c8c1a874a00616fb115456feb2b2c999dd63f6
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-26 23:55:45 +0200

    test keyboard

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,15 @@ export default function Home() {
      
       <div className="w-full flex flex-col items-center">
           <h1 className="mb-8 w-fit text-3xl"><span className='font-bold'>Welcome! </span> How can I help you?</h1>
-          <div className="flex flex-row justify-evenly text-2xl text-center size-4/5">
+          <div className="flex flex-row justify-evenly text-xl text-center size-4/5">
                 
-                <div className="bg-white rounded shadow-2xl">
+                <div className="bg-white rounded shadow-xl">
                   <div className='bg-gradient-to-r from-purple-500 to-blue-400 h-2 rounded-t'></div>
                   <div className='h-36 w-48 p-8'>
                     Find A Book
                   </div>
                 </div>
-                <div className="bg-white rounded shadow-xl">
+                <div className="bg-white rounded shadow-2xl">
                   <div className='bg-gradient-to-r from-purple-500 to-blue-400 h-2 rounded-t'></div>
                   <div className='h-36 w-48 p-8'>
                     Return Books                  
