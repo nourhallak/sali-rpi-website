7b2fcb6c743d09018102893a07f051f2e89eb0d2
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-26 23:17:00 +0200

    test keyboard

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,21 @@ import logo from '../public/Logo_2.png';
 
 export default function Home() {
   return (
-    <main className='bg-gray-100'>
+    <main className='bg-gray-100 h-[480px]'>
       <div className='w-full h-40 flex justify-center'>
         <Image src={logo} alt="" className="w-40 m-8 " />
       </div>
-      <div className="">
-        {/* <input type='text' className='border border-gray-700'></input> */}
-      <h1 className=" ml-44 mb-8 w-fit text-3xl"><span className='font-bold '>Welcome! </span> How can I help you?</h1>
-      <div className="flex flex-row justify-evenly text-2xl text-center">
-        <div className="bg-white rounded shadow-lg h-40 w-48 p-8 border-t-8 border-t-pink-600">
-          Find A Book
-        </div>
-        <div className="bg-white rounded shadow-lg h-40 w-48 p-8 border-t-8 border-t-purple-600">
-          Return Books
-        </div>
-      </div>
+     
+      <div className="w-full flex flex-col items-center">
+          <h1 className="mb-8 w-fit text-3xl"><span className='font-bold '>Welcome! </span> How can I help you?</h1>
+          <div className="flex flex-row justify-evenly text-2xl text-center size-4/5">
+                <div className="bg-white rounded shadow-lg h-40 w-48 p-8 border-t-8 border-t-pink-600">
+                  Find A Book
+                </div>
+                <div className="bg-white rounded shadow-lg h-40 w-48 p-8 border-t-8 border-t-purple-600">
+                  Return Books
+                </div>
+          </div>
       </div>
     </main>
   );
