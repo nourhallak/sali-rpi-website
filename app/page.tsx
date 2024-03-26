76c510258a055a44b1e4c39f51403191bb8b2b2a
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-26 23:18:17 +0200

    test keyboard

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ export default function Home() {
   return (
     <main className='bg-gray-100 h-[480px]'>
       <div className='w-full h-40 flex justify-center'>
-        <Image src={logo} alt="" className="w-40 m-8 " />
+        <Image src={logo} alt="" className="w-40 m-8"/>
       </div>
      
       <div className="w-full flex flex-col items-center">
-          <h1 className="mb-8 w-fit text-3xl"><span className='font-bold '>Welcome! </span> How can I help you?</h1>
+          <h1 className="mb-8 w-fit text-3xl"><span className='font-bold'>Welcome! </span> How can I help you?</h1>
           <div className="flex flex-row justify-evenly text-2xl text-center size-4/5">
                 <div className="bg-white rounded shadow-lg h-40 w-48 p-8 border-t-8 border-t-pink-600">
                   Find A Book
                 </div>
-                <div className="bg-white rounded shadow-lg h-40 w-48 p-8 border-t-8 border-t-purple-600">
+                <div className="bg-white rounded shadow-xl h-40 w-48 p-8 border-t-8 border-t-purple-600">
                   Return Books
                 </div>
           </div>
