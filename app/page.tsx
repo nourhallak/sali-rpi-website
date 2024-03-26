90c93c67954025df2f9879e3eba98f1555d0efd0
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-26 22:52:45 +0200

    test keyboard

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Image src={logo} alt="" className="w-40 m-8 " />
       </div>
       <div className="">
-        {/* <input type='text' className='border border-gray-700'></input> */}
+        <input type='text' className='border border-gray-700'></input>
       <h1 className=" ml-44 mb-8 w-fit text-3xl"><span className='font-bold '>Welcome! </span> How can I help you?</h1>
       <div className="flex flex-row justify-evenly text-2xl text-center">
         <div className="rounded shadow-lg h-40 w-48 p-8 border-t-8 border-t-pink-600">
