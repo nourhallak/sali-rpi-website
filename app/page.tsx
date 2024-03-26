e8a1d04596eec997e8da7c2ba06ab1a72aebf997
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-26 22:10:10 +0200

    Edits in the allignments

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Image src={logo} alt="" className="w-40 m-8 " />
       </div>
       <div className="">
-      <h1 className=" ml-44 mb-8 w-fit text-3xl"><span className='font-bold '>Welcome!</span> How can I help you?</h1>
+      <h1 className=" ml-44 mb-8 w-fit text-3xl"><span className='font-bold '>Welcome! </span> How can I help you?</h1>
       <div className="flex flex-row justify-evenly text-2xl text-center">
         <div className="rounded shadow-lg h-40 w-48 p-8 border-t-8 border-t-pink-600">
           Find A Book
