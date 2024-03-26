e79f74524fbd05d120a3bd83ae0d0579333c862f
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-26 22:57:01 +0200

    test keyboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-gray-100">
       <head>
       <link rel="icon" href="/icon.ico" sizes="any" />
       </head>
