afcfc3c8c2872efcd1dbfa23a153d4d0667a4082
Nour Hallak <nour.n.hallak@gmail.com>
2024-04-02 00:17:34 +0300

    edit back btn search

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -7,7 +7,7 @@ export default function Search() {
       <Header />   
       <div className='flex items-center'>
         <h1 className="m-10 w-fit text-xl">Type the name of the book:</h1>
-        <input type="text" className='border border-black h-6 w-64' />
+        <input type="text" className='border border-black h-6 w-80' />
       </div>
       <div className='flex items-end h-[260px]'>
         <Link href="../" className='m-12 p-3 border border-gray-300'>  Back to home </Link>
