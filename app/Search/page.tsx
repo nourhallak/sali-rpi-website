4b9154c225b9fca0b1475865d0e4d947386d6094
Nour Hallak <nour.n.hallak@gmail.com>
2024-04-02 00:20:17 +0300

    centered items in search page

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -5,8 +5,8 @@ export default function Search() {
   return (
     <main className='h-[480px]'>
       <Header />   
-      <div className='flex items-center'>
-        <h1 className="m-10 w-fit text-xl">Type the name of the book:</h1>
+      <div className='flex items-center justify-center'>
+        <h1 className="mt-10 mr-10 mb-10 w-fit text-xl">Type the name of the book:</h1>
         <input type="text" className='border border-black h-6 w-80' />
       </div>
       <div className='flex items-end h-[260px]'>
