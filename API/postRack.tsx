53be0c49b78440a93a55bb4c8beb893e5ad69289
Nour Hallak <nour.n.hallak@gmail.com>
2024-04-15 19:24:01 +0300

    add content type to post rack request

--- API/postRack.tsx
+++ API/postRack.tsx
@@ -4,6 +4,9 @@ import { json } from "stream/consumers";
 export default async function PostRack(distance: number) {
   const res = await fetch(`${URL}/screen/movecamera`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({ data: distance }),
   });
   return;
